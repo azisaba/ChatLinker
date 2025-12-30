66209c5d07072e7f59d10bda431a72537cf7b371
sysnote8main <sysnote8main@gmail.com>
2025-12-30 13:52:57 +0900

    feat: enable to toggle guildOnly and adminOnly

--- src/main/kotlin/net/azisaba/jdawrap/command/Variables.kt
+++ src/main/kotlin/net/azisaba/jdawrap/command/Variables.kt
@@ -1,10 +1,23 @@
 package net.azisaba.jdawrap.command
 
+import net.dv8tion.jda.api.interactions.InteractionContextType
+import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions
 import net.dv8tion.jda.api.interactions.commands.build.Commands
 import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData
 
 fun slashCommand(
     name: String,
     description: String,
+    guildOnly: Boolean = true,
+    adminOnly: Boolean = true,
     slashCommandBuilder: SlashCommandData.() -> Unit = {},
-): SlashCommandData = Commands.slash(name, description).apply { slashCommandBuilder() }
+): SlashCommandData =
+    Commands.slash(name, description).apply {
+        if (guildOnly) {
+            setContexts(InteractionContextType.GUILD)
+        }
+        if (adminOnly) {
+            defaultPermissions = DefaultMemberPermissions.DISABLED
+        }
+        slashCommandBuilder()
+    }
